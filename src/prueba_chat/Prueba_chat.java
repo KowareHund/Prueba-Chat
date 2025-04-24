09e4c3b0d24e50be5bfe7db6262656d497659836
JavierFelipe <jarodriguezac@unal.edu.co>
2025-04-24 15:05:10 -0500

    1. Inicio de chat

---
+++ src/prueba_chat/Prueba_chat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba_chat;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Prueba_chat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        1. Hola
+    }
+    
+}
